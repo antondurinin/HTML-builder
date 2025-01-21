554758880628166538ad6c4e6ac58bb1cfdc07c4
Anton Durinin <antondurinin@gmail.com>
2025-01-22 00:28:56 +0200

    refactor: change names in 01

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const { pipeline } = require('stream');
 
-const pathTofile = path.join(__dirname, 'text.txt');
-const stream = fs.createReadStream(pathTofile, 'utf-8');
+const pathToFile = path.join(__dirname, 'text.txt');
+const stream = fs.createReadStream(pathToFile, 'utf-8');
 
 pipeline(stream, process.stdout, (error) => { 
     if (error) {
