30664593d428f49d0f64ae22b3e892b3c4133b89
Anton Durinin <antondurinin@gmail.com>
2025-01-21 16:17:51 +0200

    feat: resolve 3rd task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,14 +1,36 @@
-import { readdir } from 'node:fs/promises';
-import { path } from "path";
+const { readdir, stat } = require('node:fs/promises');
+const path = require('path');
+
+async function readFilesInDir(dirPath) {
+  try {
+    return await readdir(dirPath, { withFileTypes: true });
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 const dirName = 'secret-folder';
 const dirPath = path.join(__dirname, dirName);
 
-try {
-  const files = await readdir(dirPath, { withFileTypes: true });
-  files.array.forEach((element) => {
-    console.log(element);
-  });
-} catch (error) {
-  console.error(error);
-}
+(async () => {
+  try {
+    const files = await readFilesInDir(dirPath);
+    for (let i = 0; i < files.length; i++) {
+      const element = files[i];
+      (async () => {
+        const elPath = path.join(element.path, element.name);
+        try {
+          const stats = await stat(elPath);          
+          if (stats.isFile()) {
+            const { name, ext } = path.parse(elPath);
+            console.log(`${name}-${ext.slice(1)}-${stats.size/1000}kb`);
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      })();
+    }
+  } catch (error) {
+    console.error(error);
+  }
+})();
